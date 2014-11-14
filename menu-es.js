70bb5a457fb0b3b3d07629de2a0ba1da5a806921
Alejandro Oviedo <alejandro.oviedo.g@gmail.com>
2014-11-14 00:39:01 -0300

    added menu-es.js

---
+++ menu-es.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"INTRODUCTION": "Introducción",
+	"VARIABLES": "Variables",
+	"STRINGS": "Strings",
+	"STRING LENGTH": "Longitud de strings",
+	"REVISING STRINGS": "Modificando strings",
+	"NUMBERS":"Números",
+	"ROUNDING NUMBERS":"Redondeando números",
+	"NUMBER TO STRING":"Números a string",
+	"IF-STATEMENT":"Bloque condicional",
+	"FOR LOOP":"Bucles",
+	"ARRAYS":"Arrays",
+	"ARRAY FILTERING":"Filtrado de arrays",
+	"LOOPING THROUGH ARRAYS":"Recorriendo arrays",
+	"OBJECTS":"Objetos",
+	"OBJECT PROPERTIES":"Propiedades de objetos",
+	"FUNCTIONS":"Funciones",
+	"FUNCTION-ARGUMENTS":"Argumentos de funciones"
+};
